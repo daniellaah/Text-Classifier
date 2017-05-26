9fb849c62690300169191359d2d91bb03202715d
daniellaah <bobogao223@163.com>
2017-05-26 20:10:55 +0800

    new-classifier v0

--- News-Classifier_v0/model_training_save.py
+++ News-Classifier_v0/model_training_save.py
@@ -8,32 +8,19 @@ import matplotlib.pyplot as plt
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
 
-def get_stopwords(stopwords_file):
-    """返回所有停止词
-    Args:
-        stopwords_file: 停止词文件路径
-    """
-    stopwords_set = set()
-    with open(stopwords_file, 'r') as f:
-        for line in f.readlines():
-            stopwords_set.add(line.strip())
-    return stopwords_set
-
-def words_extract(news_folder, stopwords_file="stopwords.txt"):
+def words_extract(news_folder):
     """从所有文件内容提取词
     Args:
-        news_data_folder:
-            train/
-                财经/
-                体育/
-                娱乐/
-        stopwords_file: 停止词文件路径
+        news_folder/
+            财经/
+            体育/
+            娱乐/
     """
     subfolder_list = [subfolder for subfolder in os.listdir(news_folder) \
                         if os.path.isdir(os.path.join(news_folder, subfolder))]
     data_list = [] # element: ([word1, word2, ...], "财经")
 
-    jieba.enable_parallel(8)
+    jieba.enable_parallel(4)
     # 遍历每个类别下的新闻
     for subfolder in subfolder_list:
         news_class = subfolder
@@ -48,7 +35,24 @@ def words_extract(news_folder, stopwords_file="stopwords.txt"):
     jieba.disable_parallel()
     return data_list
 
-def get_feature_words(train_data_list, stopwords_file="stopwords.txt"):
+def get_stopwords(stopwords_file="stopwords.txt"):
+    """返回所有停止词
+    Args:
+        stopwords_file: 停止词文件路径
+    """
+    stopwords_set = set()
+    with open(stopwords_file, 'r') as f:
+        for line in f.readlines():
+            stopwords_set.add(line.strip())
+    return stopwords_set
+
+def get_feature_words(train_data_list, size=1000, stopwords_file="stopwords.txt"):
+    """从训练集提取待选特征词
+    Args:
+        train_data_list:
+            element: ([word1, word2, ...], "财经")
+        stopwords_file: 停止词文件路径
+    """
     stopwords = get_stopwords(stopwords_file)
     feature_words_dict = {}
     for element in train_data_list:
@@ -60,33 +64,27 @@ def get_feature_words(train_data_list, stopwords_file="stopwords.txt"):
                     feature_words_dict[word] = 1
     feature_words_tuple = sorted(feature_words_dict.items(), key=lambda x:x[1], reverse=True)
     feature_words = list(list(zip(*feature_words_tuple))[0])
-    return feature_words
+    return feature_words[:size] if len(feature_words) > size else feature_words
 
 def train_test_extract(train_data, test_data, feature_words):
+    """从训练数据与测试数据提取 X_train, y_train, X_test, y_test
+    Args:
+        train_data: 训练数据
+        test_data: 测试数据
+        feature_words: 特征词
+    """
     X_train = [[1 if word in element[0] else 0 for word in feature_words] for element in train_data]
     y_train = [element[1] for element in train_data]
     X_test = [[1 if word in element[0] else 0 for word in feature_words] for element in test_data]
     y_test = [element[1] for element in test_data]
     return X_train, y_train, X_test, y_test
 
-def predictWithFile(classifier, news_file, feature_words):
-    with open(news_file) as f:
-        news = f.read().strip()
-    word_list = jieba.lcut(news)
-    x = [1 if word in word_list else 0 for word in feature_words]
-    return classifier.predict(x)
-
-def predictWithContent(classifier, news_content, feature_words):
-    word_list = jieba.lcut(news_content)
-    x = [1 if word in word_list else 0 for word in feature_words]
-    return classifier.predict(x)
-
 if __name__ == '__main__':
     start_time = time.time()
     train_data = words_extract('train_test_data/train')
     test_data = words_extract('train_test_data/test')
-    feature_words = get_feature_words(train_data, stopwords_file="stopwords.txt")
-    X_train, y_train, X_test, y_test = train_test_extract(train_data, test_data, feature_words[:1000])
+    feature_words = get_feature_words(train_data, size=1000, stopwords_file="stopwords.txt")
+    X_train, y_train, X_test, y_test = train_test_extract(train_data, test_data, feature_words)
     clf = MultinomialNB().fit(X_train, y_train)
     test_accuracy = clf.score(X_test, y_test)
     print("用时%ss" % str(time.time()-start_time))
